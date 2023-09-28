b5031f6afa1843e4e5965bc1d9baaa846b65b1b5
heeju1212 <heeju4746@gmail.com>
2023-09-28 13:31:01 +0900

    4번

--- main.c
+++ main.c
@@ -4,12 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
 	int a;
-	
-	printf("input seconds :");
+	printf("input a year :");
 	scanf("%i",&a);
-	printf("%i seconds - %i : %i \n",a,a/60,a%60);
+	
+	if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0))
+	{
+		printf("%i is leap year.\n",a);	
+	}
+	else {
+		printf("%i is not leap year\n",a);	
+	}
 	
 	system("PAUSE");
 	return 0;
